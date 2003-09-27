43d619062134379ad38f123e6239b0bb48d33506
fmidgley <fmidgley>
2003-09-27 21:43:19 +0000

    Added -name and -imageSourceView

--- Standard Plugins/GoogleImageSourceController.m
+++ Standard Plugins/GoogleImageSourceController.m
@@ -2,4 +2,18 @@
 
 @implementation GoogleImageSourceController
 
++ (NSString *)name
+{
+	return @"Google";
+}
+
+
+- (NSView *)imageSourceView
+{
+	if (!_imageSourceView)
+		[NSBundle loadNibNamed:@"Google Image Source" owner:self];
+	return _imageSourceView;
+}
+
+
 @end
