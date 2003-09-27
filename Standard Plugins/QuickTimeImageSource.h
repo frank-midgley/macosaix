8115cbf13975982cd17eb4ca6003efe15eeec220
fmidgley <fmidgley>
2003-09-27 21:39:44 +0000

    Initial CVS load.

---
+++ Standard Plugins/QuickTimeImageSource.h
@@ -0,0 +1,21 @@
+//
+//  QuickTimeImageSource.h
+//  MacOSaiX
+//
+//  Created by Frank Midgley on Wed Mar 13 2002.
+//  Copyright (c) 2001 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <QuickTime/QuickTime.h>
+
+#import <MacOSaiXPlugins/ImageSource.h>
+
+@interface QuickTimeImageSource : ImageSource
+{
+    NSString	*_moviePath;
+	Movie		_movie;
+	TimeValue	_curTimeValue;
+}
+
+@end
---
+++ Standard Plugins/QuickTimeImageSource.m
@@ -0,0 +1,125 @@
+#import <AppKit/AppKit.h>
+#import <QuickTime/QuickTime.h>
+#import "QuickTimeImageSource.h"
+
+
+@implementation QuickTimeImageSource
+
+
+- (id)initWithPath:(NSString *)path
+{
+	self = [super init];
+	if (self)
+	{
+		FSRef		movieRef;
+		OSStatus	status = FSPathMakeRef([path fileSystemRepresentation], &movieRef, NULL);
+	
+		_moviePath = [path retain];
+		_curTimeValue = 0;
+		if (status == noErr)
+		{
+			FSSpec	movieSpec;
+			OSErr	err = FSGetCatalogInfo(&movieRef, kFSCatInfoNone, NULL, NULL, &movieSpec, NULL);
+		
+			if (err == noErr)
+			{
+				short	theRefNum;
+	
+				EnterMovies();
+				err = OpenMovieFile(&movieSpec, &theRefNum, fsRdPerm);
+				if (err == noErr)
+				{
+					err = NewMovieFromFile(&_movie, theRefNum, 0, NULL, newMovieActive, NULL);
+					
+					if (err == noErr)
+					{
+						Rect		movieBounds;
+						
+						GetMovieBox(_movie, &movieBounds);
+						OffsetRect(&movieBounds, -movieBounds.left, -movieBounds.top);
+						SetMovieBox(_movie, &movieBounds);
+						NSLog(@"Movie size:%d, %d", movieBounds.right, movieBounds.bottom);
+					}
+					
+					CloseMovieFile(theRefNum);
+				}
+			}
+		}
+		if (!_movie)
+		{
+			[self autorelease];
+			self = nil;
+		}
+	}
+    return self;
+}
+
+
+	// return the image to be displayed in the list of image sources
+- (NSImage *)image;
+{
+    return [[NSWorkspace sharedWorkspace] iconForFile:_moviePath];
+}
+
+
+	// return the text to be displayed in the list of image sources
+- (NSString *)descriptor
+{
+    return _moviePath;
+}
+
+
+- (BOOL)hasMoreImages
+{
+	return _curTimeValue < GetMovieDuration(_movie);
+}
+
+
+- (id)nextImageIdentifier
+{
+	_curTimeValue += 100;
+	return [NSNumber numberWithLong:_curTimeValue];
+}
+
+
+- (NSImage *)imageForIdentifier:(id)identifier
+{
+	TimeValue	requestedTime = [identifier longValue];
+	
+	if (requestedTime > GetMovieDuration(_movie)) return nil;
+	
+	PicHandle	picHandle = GetMoviePict(_movie, requestedTime);
+	NSImage		*imageAtTimeValue = nil;
+	Rect		movieBounds = {0, 0, 0, 0};
+	
+	if (picHandle)
+	{
+		NSPICTImageRep	*imageRep = [NSPICTImageRep imageRepWithData:[NSData dataWithBytes:*picHandle 
+																					length:GetHandleSize((Handle)picHandle)]];
+		
+		movieBounds.top += 1;
+		if (imageAtTimeValue = [[[NSImage alloc] initWithSize:[imageRep size]] autorelease])
+		{
+			[imageAtTimeValue setScalesWhenResized:YES];
+			NS_DURING
+				[imageAtTimeValue lockFocus];
+					[imageRep drawAtPoint:NSMakePoint(0,0)];
+				[imageAtTimeValue unlockFocus];
+			NS_HANDLER
+				NSLog(@"QuickTime Image Source: Could not lock focus on image");
+			NS_ENDHANDLER
+		}
+		
+		KillPicture(picHandle);
+	}
+    return imageAtTimeValue;
+}
+
+
+- (void)dealloc
+{
+	[_moviePath release];
+}
+
+
+@end
