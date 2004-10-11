3dcbdabe35ee29b4135720bf8bdcdfd9466c50ab
Frank Midgley <knarf@mac.com>
2004-10-11 01:50:03 +0000

    Now remembering settings in user defaults. Fixed 90->100->90 bug.

--- Standard Plugins/RectangularTilesSetupController.m
+++ Standard Plugins/RectangularTilesSetupController.m
@@ -3,11 +3,16 @@
 //  MacOSaiX
 //
 //  Created by Frank Midgley on Thu Jan 23 2003.
-//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2003-2004 Frank M. Midgley. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 #import "RectangularTilesSetupController.h"
+
+
+@interface RectangularTilesSetupController (PrivateMethods)
+- (void)createTileOutlines;
+@end
 
 
 @implementation RectangularTilesSetupController
@@ -24,65 +29,86 @@
 {
 	if (!_setupView)
 	{
-		NSUserDefaults	*defaults = [NSUserDefaults standardUserDefaults];
-		NSString		*previousTilesWide = [defaults objectForKey:@"Tiles Wide"],
-						*previousTilesHigh = [defaults objectForKey:@"Tiles High"];
-		
+			// First load our nib.
 		[NSBundle loadNibNamed:@"RectangularTilesSetup" owner:self];
 
-		[_tilesAcrossStepper setStringValue:(previousTilesWide ? previousTilesWide : @"40")];
-		_tilesWide = [_tilesAcrossStepper intValue];
-		[_tilesAcrossView setIntValue:_tilesWide];
-		[_tilesDownStepper setStringValue:(previousTilesHigh ? previousTilesHigh : @"40")];
-		_tilesHigh = [_tilesDownStepper intValue];
-		[_tilesDownView setIntValue:_tilesHigh];
-
+			// Update the nib with the user's last used settings.
+		NSDictionary	*plugInDefaults = [[NSUserDefaults standardUserDefaults] objectForKey:@"Rectangular Tiles"];
+		
+		int				previousTilesWide = [[plugInDefaults objectForKey:@"Tiles Wide"] intValue];
+		tilesWide = (previousTilesWide > 0 && previousTilesWide <= [_tilesAcrossStepper maxValue]) ? 
+						previousTilesWide : [_tilesAcrossStepper intValue];
+		[_tilesAcrossStepper setIntValue:tilesWide];
+		[_tilesAcrossView setIntValue:tilesWide];
+		
+		int				previousTilesHigh = [[plugInDefaults objectForKey:@"Tiles High"] intValue];
+		tilesHigh = (previousTilesHigh > 0 && previousTilesHigh <= [_tilesDownStepper maxValue]) ? 
+						previousTilesHigh : [_tilesDownStepper intValue];
+		[_tilesDownStepper setIntValue:tilesHigh];
+		[_tilesDownView setIntValue:tilesHigh];
+		
+			// Create an initial set of tile outlines based on the current settings.
 		[self createTileOutlines];
 	}
+	
 	return _setupView;
+}
+
+
+- (void)updatePlugInDefaults
+{
+	[[NSUserDefaults standardUserDefaults] setObject:[NSDictionary dictionaryWithObjectsAndKeys:
+														[NSNumber numberWithInt:tilesWide], @"Tiles Wide", 
+														[NSNumber numberWithInt:tilesHigh], @"Tiles High", 
+														nil]
+											  forKey:@"Rectangular Tiles"];
 }
 
 
 - (IBAction)setTilesAcross:(id)sender
 {
+		// Jump by 10 if the user had the option key down, by one otherwise.
 	if (([[NSApp currentEvent] modifierFlags] & NSAlternateKeyMask) != 0)
 	{
-		if ([_tilesAcrossStepper intValue] > _tilesWide)
-			[_tilesAcrossStepper setIntValue:MIN(_tilesWide + 10, [_tilesAcrossStepper maxValue])];
-		else
-			[_tilesAcrossStepper setIntValue:MAX(_tilesWide - 10, [_tilesAcrossStepper minValue])];
+		if ([_tilesAcrossStepper intValue] > tilesWide)
+			[_tilesAcrossStepper setIntValue:MIN(tilesWide + 10, [_tilesAcrossStepper maxValue])];
+		else if ([_tilesAcrossStepper intValue] < tilesWide)
+			[_tilesAcrossStepper setIntValue:MAX(tilesWide - 10, [_tilesAcrossStepper minValue])];
 	}
-    _tilesWide = [_tilesAcrossStepper intValue];
-    [_tilesAcrossView setIntValue:_tilesWide];
+    tilesWide = [_tilesAcrossStepper intValue];
+    [_tilesAcrossView setIntValue:tilesWide];
+	
+	[self updatePlugInDefaults];
 	[self createTileOutlines];
 }
 
 
 - (IBAction)setTilesDown:(id)sender
 {
+		// Jump by 10 if the user had the option key down, by one otherwise.
 	if (([[NSApp currentEvent] modifierFlags] & NSAlternateKeyMask) != 0)
 	{
-		if ([_tilesDownStepper intValue] > _tilesHigh)
-			[_tilesDownStepper setIntValue:MIN(_tilesHigh + 10, [_tilesDownStepper maxValue])];
-		else
-			[_tilesDownStepper setIntValue:MAX(_tilesHigh - 10, [_tilesDownStepper minValue])];
+		if ([_tilesDownStepper intValue] > tilesHigh)
+			[_tilesDownStepper setIntValue:MIN(tilesHigh + 10, [_tilesDownStepper maxValue])];
+		else if ([_tilesDownStepper intValue] < tilesHigh)
+			[_tilesDownStepper setIntValue:MAX(tilesHigh - 10, [_tilesDownStepper minValue])];
 	}
-    _tilesHigh = [_tilesDownStepper intValue];
-    [_tilesDownView setIntValue:_tilesHigh];
+    tilesHigh = [_tilesDownStepper intValue];
+    [_tilesDownView setIntValue:tilesHigh];
+	
+	[self updatePlugInDefaults];
 	[self createTileOutlines];
 }
 
 
 - (void)createTileOutlines
 {
-	if (_tilesWide > 0 && _tilesHigh > 0)
-	{
-		int				x, y;
-		NSRect			tileRect = NSMakeRect(0, 0, 1.0 / _tilesWide, 1.0 / _tilesHigh);
-		NSMutableArray	*tileOutlines = [NSMutableArray arrayWithCapacity:x * y];
+	int				x, y;
+	NSRect			tileRect = NSMakeRect(0, 0, 1.0 / tilesWide, 1.0 / tilesHigh);
+	NSMutableArray	*tileOutlines = [NSMutableArray arrayWithCapacity:x * y];
 		
-    for (y = _tilesHigh - 1; y >= 0; y--)
-		for (x = 0; x < _tilesWide; x++)
+    for (y = tilesHigh - 1; y >= 0; y--)
+		for (x = 0; x < tilesWide; x++)
 			{
 				tileRect.origin.x = x * tileRect.size.width;
 				tileRect.origin.y = y * tileRect.size.height;
@@ -95,8 +121,7 @@
 				[tileOutlines addObject:[NSBezierPath bezierPathWithRect:tileRect]];
 			}
 		
-		[self setTileOutlines:tileOutlines];
-	}
+	[self setTileOutlines:tileOutlines];
 }
 
 
