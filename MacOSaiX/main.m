44907d777448d62ac20f209baca45a4e23ec07f7
Frank Midgley <knarf@mac.com>
2004-12-19 22:32:58 +0000

    Now re-nicing to play nice with others.

--- MacOSaiX/main.m
+++ MacOSaiX/main.m
@@ -1,8 +1,11 @@
 #import <Cocoa/Cocoa.h>
-//#import <Foundation/Debug.h>
+#import <sys/time.h>
+#import <sys/resource.h>
 
 int main(int argc, const char *argv[])
 {
-//    NSZombieEnabled = YES;
+		// Re-nice ourself to the lowest priority so we don't take over the user's machine.
+	setpriority(PRIO_PROCESS, 0, PRIO_MAX);
+	
     return NSApplicationMain(argc, argv);
 }
