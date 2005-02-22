6f332d4e383e0f53927956bc0658f30e6bb55f14
Frank Midgley <knarf@mac.com>
2005-02-22 05:04:00 +0000

    Now performing update check on launch.

--- MacOSaiX/MacOSaiX.m
+++ MacOSaiX/MacOSaiX.m
@@ -3,6 +3,8 @@
 #import "MacOSaiXTileShapes.h"
 #import "MacOSaiXImageSource.h"
 #import "MacOSaiXDocument.h"
+#import "MacPADSocket.h"
+#import "MacOSaiXUpdateAvailableController.h"
 #import <pthread.h>
 
 
@@ -24,9 +26,9 @@
 {
 	if (self = [super init])
 	{
-		tileShapesClasses = [[NSMutableArray arrayWithCapacity:1] retain];
-		imageSourceClasses = [[NSMutableArray arrayWithCapacity:4] retain];
-		loadedPlugInPaths = [[NSMutableArray arrayWithCapacity:5] retain];
+		tileShapesClasses = [[NSMutableArray array] retain];
+		imageSourceClasses = [[NSMutableArray array] retain];
+		loadedPlugInPaths = [[NSMutableArray array] retain];
 		
 //		[[NSNotificationCenter defaultCenter] addObserver:self
 //												 selector:@selector(documentDidFinishSaving:)
@@ -39,17 +41,70 @@
 
 - (void)applicationWillFinishLaunching:(NSNotification *)notification
 {
+	NSDate				*dateOfNextUpdateCheck = [[NSUserDefaults standardUserDefaults] objectForKey:@"Update Check After Date"];
+	NSString			*versionToSkip = [[NSUserDefaults standardUserDefaults] objectForKey:@"Update Check Version to Skip"],
+						*currentVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+	
+	if (!dateOfNextUpdateCheck || [dateOfNextUpdateCheck timeIntervalSinceNow] <= 0)
+	{
+		[[NSUserDefaults standardUserDefaults] removeObjectForKey:@"Update Check After Date"];
+		
+		MacPADSocket	*macPAD = [[MacPADSocket alloc] init];
+		[macPAD setDelegate:self];
+		
+		if ([macPAD compareVersion:currentVersion toVersion:versionToSkip] != NSOrderedDescending)
+		{
+			[[NSUserDefaults standardUserDefaults] removeObjectForKey:@"Update Check Version to Skip"];
+			versionToSkip = nil;
+		}
+		
+		if (versionToSkip)
+			[macPAD performCheck:[NSURL URLWithString:@"http://homepage.mac.com/knarf/MacOSaiX/Version.plist"]
+					withVersion:versionToSkip];
+		else
+			[macPAD performCheckWithURL:[NSURL URLWithString:@"http://homepage.mac.com/knarf/MacOSaiX/Version.plist"]];
+	}
+}
+
+
+- (void)macPADErrorOccurred:(NSNotification *)notification
+{
+	MacPADSocket	*macPAD = [notification object];
+//	NSDictionary	*updateCheckInfo = [notification userInfo];
+	
+	[macPAD release];
+}
+
+
+- (void)macPADCheckFinished:(NSNotification *)notification
+{
+	MacPADSocket	*macPAD = [notification object];
+	NSDictionary	*updateCheckInfo = [notification userInfo];
+	
+	switch ([[updateCheckInfo objectForKey:MacPADErrorCode] intValue])
+	{
+		case kMacPADResultNoNewVersion:		// No new version available. Not an error
+		case kMacPADResultMissingValues:	// One or both arguments to performCheck: were nil
+		case kMacPADResultInvalidURL:		// URL was invalid or could not be contacted
+		case kMacPADResultInvalidFile:		// XML file was missing or not well-formed
+		case kMacPADResultBadSyntax:		// Version info was missing from XML file
+			break;
+		case kMacPADResultNewVersion:		// New version is available.
+		{
+			MacOSaiXUpdateAvailableController	*controller = [[MacOSaiXUpdateAvailableController alloc] 
+																	initWithMacPADSocket:macPAD];
+			[controller showWindow:self];
+			break;
+		}
+	}
+	
+	[macPAD release];
+	macPAD = nil;
 }
 
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification
 {
-	// TODO: version check
-	
-		// Do an initial discovery of plug-ins
-		// (Now done lazily in MacOSaiXWindowController.)
-	//[self discoverPlugIns];
-
 		// To provide a service:
     //[NSApp setServicesProvider:[[EncryptoClass alloc] init]];
 }
