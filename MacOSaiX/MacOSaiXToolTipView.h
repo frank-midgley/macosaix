67dff7cd0af913c12f838a58ad72688776454e98
Frank Midgley <knarf@mac.com>
2006-05-15 14:32:44 +0000

    View for custom tooltips.

---
+++ MacOSaiX/MacOSaiXToolTipView.h
@@ -0,0 +1,17 @@
+//
+//  MacOSaiXToolTipView.h
+//  MacOSaiX
+//
+//  Created by Frank Midgley on 5/14/06.
+//  Copyright 2006 Frank M. Midgley. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface MacOSaiXToolTipView : NSView
+{
+
+}
+
+@end
---
+++ MacOSaiX/MacOSaiXToolTipView.m
@@ -0,0 +1,30 @@
+//
+//  MacOSaiXToolTipView.m
+//  MacOSaiX
+//
+//  Created by Frank Midgley on 5/14/06.
+//  Copyright 2006 Frank M. Midgley. All rights reserved.
+//
+
+#import "MacOSaiXToolTipView.h"
+
+
+@interface NSColor (MacOSaiX)
++ (NSColor *)toolTipColor;
+@end
+
+
+@implementation MacOSaiXToolTipView
+
+
+- (void)drawRect:(NSRect)rect
+{
+	[[[NSColor yellowColor] highlightWithLevel:0.75] set];
+	NSRectFill(rect);
+	
+	[[NSColor grayColor] set];
+	NSFrameRect([self bounds]);
+}
+
+
+@end
