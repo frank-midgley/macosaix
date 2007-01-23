9b03e5f62c66256ee7b2237746cfb50e9d2d7834
Frank Midgley <knarf@mac.com>
2007-01-23 04:36:59 +0000

    Tile shape outlines are now in target image space.

--- MacOSaiX/MacOSaiXTileShapes.h
+++ MacOSaiX/MacOSaiXTileShapes.h
@@ -11,8 +11,8 @@
 
 @protocol MacOSaiXTileShape <NSObject>
 
-	// This method should return a bezier path that defines the outline of the shape.  The outline is assumed to exist inside of a unit square that will be mapped to a mosaic's target image.  So, for example, the size of a rectangular tile in a 40x40 array would be {0.025, 0.025}.
-- (NSBezierPath *)unitOutline;
+	// This method should return a bezier path that defines the outline of the shape.  The outline is assumed to exist inside of a rectangle located at {0.0, 0.0} and having the size passed to the -shapesForMosaicOfSize: method below.  So, for example, an outline for a tile in a set of 40x40 rectangular tiles for a mosaic of size {600.0, 400.0} could have a bounding box of {0.0, 0.0, 15.0, 10.0}.
+- (NSBezierPath *)outline;
 
 	// This method can return the angle at which images should be drawn inside the tile, in degrees.  An angle of 0 degress will draw images drawn in their normal, upright orientation.  If nil is returned then the angle specified by the image orientation plug-in will be used instead.
 - (NSNumber *)imageOrientation;
@@ -23,6 +23,6 @@
 @protocol MacOSaiXTileShapes <MacOSaiXDataSource>
 
 	// This method should return an array of objects conforming to the MacOSaiXTileShape protocol based on the settings defined by the user.
-- (NSArray *)shapes;
+- (NSArray *)shapesForMosaicOfSize:(NSSize)mosaicSize;
 
 @end
