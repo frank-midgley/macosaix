40dc89d15d8a6cd962f0c6172d4be2f624e1c9fb
Frank Midgley <knarf@mac.com>
2007-09-06 21:30:15 +0000

    Angle offset for each tile now being calculated correctly.

--- Standard Plugins/Tile Shapes/Spiral/MacOSaiXSpiralTileShapes.m
+++ Standard Plugins/Tile Shapes/Spiral/MacOSaiXSpiralTileShapes.m
@@ -206,67 +206,81 @@
 	angle = 0.0;
 	while (radius < maxRadius)
 	{
-		// 2*pi    2*pi*r
-		// ---- =? ------
-		//  a       Rinc 
+		/*
+			We need to know how much angle the tile will span at the current radius.  The "height" of the tile will always be the radius increment.  The "width" of the tile is the length of the portion of the circle that passes through the midpoint of the tile.
+			
+			The radius at the midpoint of the tile will be:
+			
+													   / angle   1 \
+				midRadius = radius + radiusIncrement * | ----- + - |
+													   \  4pi    2 /
+			
+			and the length of the arc through the midpoint of the tile (the "width") will be:
+			
+				                                / angle \
+				arcLength = (2pi * midRadius) * | ----- |
+				                                \  2pi  /
+			
+			To preserve the aspect ratio:
+				
+				                     arcLength
+				tileAspectRatio = ---------------
+				                  radiusIncrement
+		 
+			so we can solve for the angle by combining the equations and converting to quadratic form:
+			
+				 1              /     radius        1 \
+				--- * angle^2 + | --------------- + - | * angle - tileAspectRatio = 0
+				4pi             \ radiusIncrement   2 /
+		 */
 		
-		float			tileAngle = (radius == 0.0 ? 2.0 * sqrtf(M_PI) : radiusIncrement / radius * tileAspectRatio), 
+		float			quadA = 1.0 / 4.0 / M_PI, 
+						quadB = radius / radiusIncrement + 0.5, 
+						quadC = -tileAspectRatio, 
+						tileAngle = (-quadB + sqrtf(quadB * quadB - 4.0 * quadA * quadC)) / 2.0 / quadA, 
 						nextRadius = radius + radiusIncrement * (tileAngle / 2 / M_PI), 
-						nextAngle = angle + tileAngle, 
-						midAngle = angle + tileAngle / 2.0;
-		NSPoint			innerMidPoint = NSMakePoint(midX + (radius + nextRadius + radiusIncrement * .03)  / 2.0 * cos(midAngle), 
-													midY + (radius + nextRadius + radiusIncrement * .03)  / 2.0 * sin(midAngle)), 
-						outerMidPoint = NSMakePoint(midX + ((radius + nextRadius) / 2.0 + radiusIncrement * 1.03) * cos(midAngle), 
-													midY + ((radius + nextRadius) / 2.0 + radiusIncrement * 1.03)  * sin(midAngle));
+						nextAngle = angle + tileAngle;
+		int				segments = tileAngle * 360.0 / 2.0 / M_PI;	// the number of segments with which to approximate the curve
+		if (segments < 2)
+			segments = 2;
+		float			fractionIncrement = 1.0 / segments, 
+						fraction;
 		NSBezierPath	*tileOutline = [NSBezierPath bezierPath];
 		
+			// Add a radial line going from the inner curve to the outer curve at the the smallest radius.
 		[tileOutline moveToPoint:NSMakePoint(midX + radius * cos(angle), midY + radius * sin(angle))];
 		[tileOutline lineToPoint:NSMakePoint(midX + (radius + radiusIncrement) * cos(angle), midY + (radius + radiusIncrement) * sin(angle))];
-		if (YES)
+		
+			// Add the outer curve
+		for (fraction = fractionIncrement; fraction <= 1.0 + fractionIncrement / 10.0; fraction += fractionIncrement)
 		{
-			int		segments = tileAngle * 360.0 / 2.0 / M_PI;
-			if (segments < 2)
-				segments = 2;
-			float	fractionIncrement = 1.0 / segments, 
-					fraction;
-			for (fraction = fractionIncrement; fraction <= 1.0 + fractionIncrement / 10.0; fraction += fractionIncrement)
-			{
-				float	segmentAngle = angle + tileAngle * fraction, 
-						segmentRadius = (radius + radiusIncrement) + (nextRadius - radius) * fraction;
-				
-				[tileOutline lineToPoint:NSMakePoint(midX + segmentRadius * cos(segmentAngle), midY + segmentRadius * sin(segmentAngle))];
-			}
+			float	segmentAngle = angle + tileAngle * fraction, 
+					segmentRadius = (radius + radiusIncrement) + (nextRadius - radius) * fraction;
+			
+			[tileOutline lineToPoint:NSMakePoint(midX + segmentRadius * cos(segmentAngle), midY + segmentRadius * sin(segmentAngle))];
 		}
-		else
-			[tileOutline curveToPoint:NSMakePoint(midX + (nextRadius + radiusIncrement) * cos(nextAngle), midY + (nextRadius + radiusIncrement) * sin(nextAngle)) 
-						controlPoint1:outerMidPoint
-						controlPoint2:outerMidPoint];
+		
+			// Add a radial line going from the outer curve to the inner curve at the the largest radius.
 		[tileOutline lineToPoint:NSMakePoint(midX + nextRadius * cos(nextAngle), midY + nextRadius * sin(nextAngle))];
-		if (YES)
+		
+			// Add the innner curve.
+		for (fraction = 1.0 - fractionIncrement; fraction >= 0.0; fraction -= fractionIncrement)
 		{
-			int		segments = tileAngle * 360.0 / 2.0 / M_PI;
-			if (segments < 2)
-				segments = 2;
-			float	fractionIncrement = 1.0 / segments, 
-					fraction;
-			for (fraction = 1.0 - fractionIncrement; fraction >= 0.0; fraction -= fractionIncrement)
-			{
-				float	segmentAngle = angle + tileAngle * fraction, 
-						segmentRadius = radius + (nextRadius - radius) * fraction;
-				
-				[tileOutline lineToPoint:NSMakePoint(midX + segmentRadius * cos(segmentAngle), midY + segmentRadius * sin(segmentAngle))];
-			}
+			float	segmentAngle = angle + tileAngle * fraction, 
+					segmentRadius = radius + (nextRadius - radius) * fraction;
+			
+			[tileOutline lineToPoint:NSMakePoint(midX + segmentRadius * cos(segmentAngle), midY + segmentRadius * sin(segmentAngle))];
 		}
-		else
-			[tileOutline curveToPoint:NSMakePoint(midX + radius * cos(angle), midY + radius * sin(angle)) 
-						controlPoint1:innerMidPoint
-						controlPoint2:innerMidPoint];
+		
 		[tileOutline closePath];
 		
-		float			orientation = midAngle / M_PI * -180.0 + 90.0;
-
 		if (NSIntersectsRect([tileOutline bounds], mosaicBounds))
-			[tileOutlines addObject:[MacOSaiXSpiralTileShape tileShapeWithOutline:tileOutline orientation:([self imagesFollowSpiral] ? [NSNumber numberWithFloat:orientation] : nil)]];
+		{
+			float		midAngle = angle + tileAngle / 2.0;
+			NSNumber	*orientation = ([self imagesFollowSpiral] ? [NSNumber numberWithFloat:midAngle / M_PI * -180.0 + 90.0] : nil);
+			
+			[tileOutlines addObject:[MacOSaiXSpiralTileShape tileShapeWithOutline:tileOutline orientation:orientation]];
+		}
 		
 		radius = nextRadius;
 		angle = nextAngle;
